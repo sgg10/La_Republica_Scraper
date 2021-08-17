5e1ef8bd17b685e31e07fc088826d05b9065748c
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2021-08-17 12:53:51 -0500

    Get all notices

--- larepublica_scraper/scraper.py
+++ larepublica_scraper/scraper.py
@@ -1,5 +1,7 @@
 import requests
 import lxml.html as html
+import os
+import datetime
 
 HOME_URL = "https://www.larepublica.co/"
 
@@ -10,6 +12,34 @@ XPATH = {
     "BODY": '//div[@class="html-content"]/p[not(@class)]/text()'
 }
 
+def parse_notices(link, today):
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            notice = response.content.decode("utf-8")
+            parsed = html.fromstring(notice)
+
+            try:
+                title = parsed.xpath(XPATH['TITLE'])[0]
+                title.replace('"', '')
+                summary = parsed.xpath(XPATH['SUMMARY'])[0]
+                body = parsed.xpath(XPATH['BODY'])
+            except IndexError:
+                return
+
+            with open(f"larepublica_scraper/{today}/{title}.txt", "w", encoding="utf-8") as file:
+                file.write(title)
+                file.write("\n\n")
+                file.write(summary)
+                file.write("\n\n")
+                for p in body:
+                    file.write(p)
+                    file.write("\n")
+        else:
+            raise ValueError(f'Error: {response.status_code}')
+    except ValueError as ve:
+        print(ve.message)
+
 def parse_home() -> None:
     try:
         response = requests.get(HOME_URL)
@@ -17,7 +47,13 @@ def parse_home() -> None:
             home = response.content.decode("utf-8")
             parsed = html.fromstring(home)
             links_to_notices = parsed.xpath(XPATH['LINK_TO_ARTICLE'])
-            print(links_to_notices)
+
+            today = datetime.date.today().strftime("%d-%m-%Y")
+            if not os.path.isdir(f"larepublica_scraper/{today}"):
+                os.mkdir(f"larepublica_scraper/{today}")
+
+            for link in links_to_notices:
+                parse_notices(link, today)
         else:
             raise ValueError(f'Error: {response.status_code}')
     except ValueError as ve:
